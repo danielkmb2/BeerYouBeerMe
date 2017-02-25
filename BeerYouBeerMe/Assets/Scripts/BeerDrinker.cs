d1bce1cb21f0641caa9ccab71dbdaf4b056f4eef
Mosquito <danielkmb2@gmail.com>
2017-02-25 23:23:05 +0100

    updated assets, burp effects, updated scene flow

--- BeerYouBeerMe/Assets/Scripts/BeerDrinker.cs
+++ BeerYouBeerMe/Assets/Scripts/BeerDrinker.cs
@@ -2,12 +2,21 @@ using UnityEngine;
 
 public class BeerDrinker : MonoBehaviour {
 
+	public AudioClip burp;
+
 	public int beers = 0;
 	public string beerTag = "Beer";
+
+	private AudioSource burpEmmiter;
+
+	void Start() {
+		burpEmmiter = GetComponent<AudioSource>();
+	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
 		if (collision.gameObject.CompareTag(beerTag)) {
 			beers += 1;
+			burpEmmiter.PlayOneShot(burp);
 			Destroy(collision.gameObject);
 		}
 	}
